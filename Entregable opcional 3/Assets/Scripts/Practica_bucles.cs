774e63ff2bda329d046df389a6da93d834814e58
Marc553 <marcramis2016@gmail.com>
2022-02-16 21:33:23 +0100

    .

---
+++ Entregable opcional 3/Assets/Scripts/Practica_bucles.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practica_bucles : MonoBehaviour
+{
+
+    void Start()
+    {
+        //Ejercicio1_for();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+   //FOR
+   public void Ejercicio1_for()
+   {
+     for(int i = 0; i <= 100; i += 2)
+     {
+       Debug.Log(i);
+
+     }
+   }
+
+
+    //WHILE
+    public void Ejercicio1_while()
+    {
+
+    }
+
+
+
+
+      
+
+
+
+
+}
